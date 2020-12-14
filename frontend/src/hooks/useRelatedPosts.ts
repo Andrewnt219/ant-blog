b46240ce6c420ca5fa91b73c4a68e1c40b16f1f0
Andrew <phongnguyentuan20@gmail.com>
2020-12-14 00:36:22 -0500

    added useRelatedPosts

---
+++ frontend/src/hooks/useRelatedPosts.ts
@@ -0,0 +1,34 @@
+import { NUMBER_CONSTANTS } from "@src/assets/constants/StyleConstants";
+import { sanityFetcher } from "@src/lib/swr";
+import { PostModel, RelatedPostsModel } from "@src/model/sanity";
+import { RELATED_POSTS_QUERY } from "@src/service/sanity/sanity.query";
+import { SanityClientErrorResponse } from "sanity";
+import useSWR from "swr";
+
+type Props = {
+	post: {
+		categories: PostModel["categories"];
+		_id: PostModel["_id"];
+	};
+	initialRelatedPosts: RelatedPostsModel[];
+};
+export const useRelatedPosts = ({ post, initialRelatedPosts }: Props) => {
+	// fetch relatedPosts
+	// NOTE cannot use dataservice and bind because binded params does not change
+	const { data, error } = useSWR<
+		RelatedPostsModel[],
+		SanityClientErrorResponse
+	>(
+		[
+			RELATED_POSTS_QUERY,
+			{ categorySlug: post.categories.main.slug, postId: post._id },
+		],
+		sanityFetcher,
+		{
+			initialData: initialRelatedPosts,
+			refreshInterval: NUMBER_CONSTANTS.refreshInterval,
+		}
+	);
+
+	return { data, error };
+};
